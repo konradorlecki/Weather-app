a1ce925c83220d596fd684667935f84983c6aea1
Konrad <konrad@moodup.team>
2021-01-11 18:33:05 +0100

    Add working google auto suggest, Add working add city weather

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+
+import { MapsAPILoader } from '@agm/core';
+import { select, Store } from '@ngrx/store';
+
+import { AppState } from '../../../../store/states/app.state';
+import { WeatherActions } from '../../../../store/actions';
+import { FormControl, Validators } from '@angular/forms';
+import { WeatherInterface } from '../../../../models/weatherInterface';
+import { selectCitiesWeather } from '../../../../store/selectors/weather.selectors';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('cityInput') cityInput: ElementRef;
 
-  constructor() { }
+  public cityInputFormControl = new FormControl('', [Validators.minLength(3)]);
+  public citiesWeather: WeatherInterface[];
+
+  private ngDestroyed$ = new Subject();
+
+  constructor(
+    private mapsApiLoader: MapsAPILoader,
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+    this.store.pipe(select(selectCitiesWeather))
+      .pipe(takeUntil(this.ngDestroyed$))
+      .subscribe(citiesWeather => {
+        this.citiesWeather = citiesWeather
+      })
   }
 
+  ngAfterViewInit() {
+    this.initAutoComplete();
+  }
+
+  ngOnDestroy() {
+    this.ngDestroyed$.next();
+  }
+
+  private initAutoComplete(): void {
+    this.mapsApiLoader.load().then(() => {
+      const autocomplete = new google.maps.places.Autocomplete(
+        this.cityInput.nativeElement, { types: ['(cities)'] }
+      );
+
+      autocomplete.addListener('place_changed', () => {
+        const address = autocomplete.getPlace();
+        this.cityInputFormControl.patchValue(address.name);
+      });
+    });
+  }
+
+  public onAddCity(): void {
+    if (this.cityInputFormControl.valid) {
+      this.store.dispatch(WeatherActions.getCityByName({ cityName: this.cityInputFormControl.value }));
+      this.cityInputFormControl.patchValue('');
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   weatherApiKey: 'c5dcf6851aa1ef1dd43151effc29d1d9',
-  googleApiKey: 'AIzaSyA_CcX0yM49oYohVF8XCaNZRSyv2rvZJjM',
+  googleApiKey: 'AIzaSyCbrd-6Tdg-8W1wXn_OgsQ2t4VpSU6UihA',
 };
 
 /*
